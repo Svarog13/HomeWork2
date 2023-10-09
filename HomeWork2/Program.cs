5755f4bfbaf323536cb3c96a3438d4d1ede2e176
lenovo <lenovo@DESKTOP-D0T6Q1U>
2023-10-09 22:12:35 +0300

    Add project files.

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter the Digit: ");
+int digit = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the Interest: ");
+int interest = int.Parse(Console.ReadLine());
+double result = (digit * interest) / 100;
+
+Console.WriteLine($"Interest {interest}% from digit {digit} = {result}");   
